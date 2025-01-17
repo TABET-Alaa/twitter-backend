07049dd730d0193a9a29c34503a49b706f2ce988
Alaaddine-ta <alae.tabet02@gmail.com>
2025-01-17 15:54:37 +0100

    add the updatephonenumber function PutMapping update phone

--- src/main/java/com/twitter_backend/twitter_backend/controllers/AuthenticationController.java
+++ src/main/java/com/twitter_backend/twitter_backend/controllers/AuthenticationController.java
@@ -1,13 +1,15 @@
 package com.twitter_backend.twitter_backend.controllers;
 
 
+import com.twitter_backend.twitter_backend.exceptions.EmailAlreadyTakenException;
 import com.twitter_backend.twitter_backend.modals.ApplicationUser;
+import com.twitter_backend.twitter_backend.modals.RegistrationObject;
 import com.twitter_backend.twitter_backend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.LinkedHashMap;
 
 @RestController
 @RequestMapping("/auth")
@@ -19,12 +21,28 @@ public class AuthenticationController {
         this.userService = userService;
     }
 
-    //go to http://localhost:8000/auth/register
-    @PostMapping("/register")
-        public ApplicationUser register(@RequestBody ApplicationUser user) {
-        return userService.registerUser(user);
+    @ExceptionHandler({EmailAlreadyTakenException.class})
+    public ResponseEntity<String> handleEmailTaken(){
+        return ResponseEntity<String>("The email you provided is already in use");
     }
 
+    //go to http://localhost:8000/auth/register
+    @PostMapping("/register")
+        public ApplicationUser register(@RequestBody RegistrationObject ro) {
+
+        return userService.registerUser(ro);
+    }
+
+    @PutMapping("/update/phone")
+    public ApplicationUser updatePhoneNumber(@RequestBody LinkedHashMap<String, String> body){
+        String username = body.get("username");
+        String phone = body.get("phone");
+
+        ApplicationUser user = userService.getUserByUsername(username);
+        user.setPhone(phone);
+
+        return userService.updateUser(user);
+    }
 
 
 
---
+++ src/main/java/com/twitter_backend/twitter_backend/exceptions/UserDoesNotExistException.java
@@ -0,0 +1,2 @@
+package com.twitter_backend.twitter_backend.exceptions;public class UserDoesNotExistException {
+}
